342949f17e0f5733915acd91a88095c7bf1174f4
hong-jen kao <endless-dreaming@hotmail.com>
2013-07-09 19:15:32 +0800

    first commit 經過測試能正常運作.

---
+++ admin/language/english/shipping/total_based.php
@@ -0,0 +1,19 @@
+<?php 
+// Heading
+$_['heading_title']    = '按價計費(Total Based)';
+
+// Text
+$_['text_shipping']    = '配送(Shipping)';
+$_['text_success']     = '已成功修改按價計費的設置';
+
+// Entry
+$_['entry_cost']       = '運費(Cost)';
+$_['entry_threshold']  = '門檻(Threshold)';
+$_['entry_tax_class']  = '稅務種類(Tax Class)';
+$_['entry_geo_zone']   = '適用地區(Geo Zone)';
+$_['entry_status']     = '狀態(Status)';
+$_['entry_sort_order'] = '排序(Sort Order)';
+
+// Error
+$_['error_permission'] = '您沒有權限更改按價計費的設置';
+?>
---
+++ catalog/language/english/shipping/total_based.php
@@ -0,0 +1,5 @@
+<?php
+// Text
+$_['text_title']       = '按價計費';
+$_['text_description'] = '滿%s免運費';
+?>
